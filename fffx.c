b4a0ff3d7d035b230201378a57efe2c2356b926b
Denys Nykula <nykula@ukr.net>
2020-09-05 18:30:31 +0300

    Read audio frame

--- fffx.c
+++ fffx.c
@@ -3,20 +3,28 @@
 #include "libavutil/opt.h"
 #include <SDL.h>
 int main(int argc, char **argv) {
-  AVFilterInOut *bufs[1] = {0}, *sinks[1] = {0};
+  AVFilterContext *buf[1] = {0}, *fx[1] = {0}, *sink[1] = {0};
+  AVFrame *f;
   AVFilterGraph *flt;
-  if (!(flt = avfilter_graph_alloc()) || !(*bufs = avfilter_inout_alloc()) ||
-      !(*sinks = avfilter_inout_alloc()))
+  if (!(f = av_frame_alloc()) || !(flt = avfilter_graph_alloc()) ||
+      !(*buf = avfilter_graph_alloc_filter(flt, avfilter_get_by_name("amovie"),
+                                           0)) ||
+      !(*fx = avfilter_graph_alloc_filter(flt, avfilter_get_by_name("anull"),
+                                          0)) ||
+      !(*sink = avfilter_graph_alloc_filter(
+            flt, avfilter_get_by_name("abuffersink"), 0)))
     return printf("bad mem\n"), 1;
-  (*bufs)->filter_ctx =
-      avfilter_graph_alloc_filter(flt, avfilter_get_by_name("amovie"), 0);
-  av_opt_set((*bufs)->filter_ctx, "filename", argv[argc - 1], 1);
-  avfilter_init_str((*bufs)->filter_ctx, 0), (*bufs)->name = av_strdup("in");
-  avfilter_graph_create_filter(
-      &(*sinks)->filter_ctx, avfilter_get_by_name("abuffersink"), 0, 0, 0, flt);
-  (*sinks)->name = av_strdup("out");
-  if (avfilter_graph_parse_ptr(flt, "anull", sinks, bufs, 0) < 0 ||
-      avfilter_graph_config(flt, 0) < 0)
+
+  av_opt_set(*buf, "filename", argv[argc - 1], 1);
+  if (avfilter_init_str(*buf, 0) < 0 || avfilter_init_str(*fx, 0) < 0 ||
+      avfilter_init_str(*sink, 0) < 0 || avfilter_link(*buf, 0, *fx, 0) < 0 ||
+      avfilter_link(*fx, 0, *sink, 0) < 0 || avfilter_graph_config(flt, 0) < 0)
     return printf("bad graph\n"), 1;
-  printf("%s\n", argv[argc - 1]);
+
+  do {
+    if (av_buffersink_get_frame(*sink, f) < 0)
+      continue;
+    printf("%s pts=%f\n", argv[argc - 1],
+           f->pts * av_q2d(av_buffersink_get_time_base(*sink)));
+  } while (fgetc(stdin) >= 0);
 }
