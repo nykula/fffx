a91436567bb18723f9ff97f56c8aafec8cda8a19
Denys Nykula <nykula@ukr.net>
2020-09-06 19:35:31 +0300

    Mix wet reverb with split dry

--- fffx.c
+++ fffx.c
@@ -3,35 +3,43 @@
 #include "libavutil/opt.h"
 #include <SDL.h>
 int main(int argc, char **argv) {
-  AVFilterContext *buf[1] = {0}, *room[1] = {0}, *sink[1] = {0}, *verb[1] = {0};
+  AVFilterContext *dry[1] = {0}, *drysplit[1] = {0}, *room[1] = {0},
+                  *sink[1] = {0}, *verb[1] = {0}, *verbwet[1] = {0};
   AVFrame *f;
   AVFilterGraph *flt;
   SDL_cond *wait = 0;
   SDL_mutex *wait$ = 0;
   SDL_AudioSpec want = {0};
   if (!(f = av_frame_alloc()) || !(flt = avfilter_graph_alloc()) ||
-      !(*buf = avfilter_graph_alloc_filter(flt, avfilter_get_by_name("amovie"),
+      !(*dry = avfilter_graph_alloc_filter(flt, avfilter_get_by_name("amovie"),
                                            0)) ||
+      !(*drysplit = avfilter_graph_alloc_filter(
+            flt, avfilter_get_by_name("asplit"), 0)) ||
       !(*room = avfilter_graph_alloc_filter(flt, avfilter_get_by_name("amovie"),
                                             0)) ||
       !(*sink = avfilter_graph_alloc_filter(
             flt, avfilter_get_by_name("abuffersink"), 0)) ||
-      !(*verb = avfilter_graph_alloc_filter(flt, avfilter_get_by_name("afir"),
+      !(*verb = avfilter_graph_alloc_filter(flt, avfilter_get_by_name("amix"),
                                             0)) ||
+      !(*verbwet = avfilter_graph_alloc_filter(
+            flt, avfilter_get_by_name("afir"), 0)) ||
       !(wait = SDL_CreateCond()) || !(wait$ = SDL_CreateMutex()) ||
       SDL_LockMutex(wait$))
     return printf("bad mem\n"), 1;
 
-  if (av_opt_set(*buf, "filename", argv[argc - 1], 1) < 0 ||
-      av_opt_set_int(*buf, "loop", 0, 1) < 0 ||
+  if (av_opt_set(*dry, "filename", argv[argc - 1], 1) < 0 ||
+      av_opt_set_int(*dry, "loop", 0, 1) < 0 ||
       av_opt_set(*room, "filename", argv[argc - 2], 1) < 0 ||
       av_opt_set_int_list(*sink, "sample_fmts", ((int[]){1, -1}), -1, 1) < 0 ||
-      av_opt_set_int(*verb, "dry", 10, 1) < 0 ||
-      av_opt_set_int(*verb, "wet", 10, 1) < 0 ||
-      avfilter_init_str(*buf, 0) < 0 || avfilter_init_str(*room, 0) < 0 ||
-      avfilter_init_str(*sink, 0) < 0 || avfilter_init_str(*verb, 0) < 0 ||
-      avfilter_link(*buf, 0, *verb, 0) < 0 ||
-      avfilter_link(*room, 0, *verb, 1) < 0 ||
+      av_opt_set(*verb, "weights", "20 10", 1) < 0 ||
+      avfilter_init_str(*dry, 0) < 0 || avfilter_init_str(*drysplit, 0) < 0 ||
+      avfilter_init_str(*room, 0) < 0 || avfilter_init_str(*sink, 0) < 0 ||
+      avfilter_init_str(*verb, 0) < 0 || avfilter_init_str(*verbwet, 0) < 0 ||
+      avfilter_link(*dry, 0, *drysplit, 0) < 0 ||
+      avfilter_link(*drysplit, 0, *verbwet, 0) < 0 ||
+      avfilter_link(*room, 0, *verbwet, 1) < 0 ||
+      avfilter_link(*drysplit, 1, *verb, 0) < 0 ||
+      avfilter_link(*verbwet, 0, *verb, 1) < 0 ||
       avfilter_link(*verb, 0, *sink, 0) < 0 ||
       avfilter_graph_config(flt, 0) < 0)
     return printf("bad graph\n"), 1;
