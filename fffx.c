fa7c3f5bb7aa7dc74bc835fe54af5d2f168219fd
Denys Nykula <nykula@ukr.net>
2020-09-07 21:16:41 +0300

    Bandpass and nullsink for debug

--- fffx.c
+++ fffx.c
@@ -2,29 +2,25 @@
 #include "libavfilter/buffersink.h"
 #include "libavutil/opt.h"
 #include <SDL.h>
+static struct TT { AVFilterGraph *g; } TT;
+int fltnew(AVFilterContext **f, const char *n) {
+  return !(*f = avfilter_graph_alloc_filter(TT.g, avfilter_get_by_name(n), 0));
+}
 int main(int argc, char **argv) {
-  AVFilterContext *dry[1] = {0}, *drysplit[1] = {0}, *room[1] = {0},
-                  *sink[1] = {0}, *verb[1] = {0}, *verbwet[1] = {0};
+  AVFilterContext *band[1] = {0}, *dry[1] = {0}, *nul[1] = {0},
+                  *nulsink[1] = {0}, *room[1] = {0}, *sink[1] = {0},
+                  *verb[1] = {0}, *verbsplit[1] = {0}, *verbwet[1] = {0};
   AVFrame *f;
-  AVFilterGraph *flt;
   SDL_cond *wait = 0;
   SDL_mutex *wait$ = 0;
   SDL_AudioSpec want = {0};
-  if (!(f = av_frame_alloc()) || !(flt = avfilter_graph_alloc()) ||
-      !(*dry = avfilter_graph_alloc_filter(flt, avfilter_get_by_name("amovie"),
-                                           0)) ||
-      !(*drysplit = avfilter_graph_alloc_filter(
-            flt, avfilter_get_by_name("asplit"), 0)) ||
-      !(*room = avfilter_graph_alloc_filter(flt, avfilter_get_by_name("amovie"),
-                                            0)) ||
-      !(*sink = avfilter_graph_alloc_filter(
-            flt, avfilter_get_by_name("abuffersink"), 0)) ||
-      !(*verb = avfilter_graph_alloc_filter(flt, avfilter_get_by_name("amix"),
-                                            0)) ||
-      !(*verbwet = avfilter_graph_alloc_filter(
-            flt, avfilter_get_by_name("afir"), 0)) ||
-      !(wait = SDL_CreateCond()) || !(wait$ = SDL_CreateMutex()) ||
-      SDL_LockMutex(wait$))
+  if (!(f = av_frame_alloc()) || !(TT.g = avfilter_graph_alloc()) ||
+      fltnew(band, "bandpass") || fltnew(dry, "amovie") ||
+      fltnew(nul, "anullsrc") || fltnew(nulsink, "anullsink") ||
+      fltnew(room, "amovie") || fltnew(sink, "abuffersink") ||
+      fltnew(verb, "amix") || fltnew(verbsplit, "asplit") ||
+      fltnew(verbwet, "afir") || !(wait = SDL_CreateCond()) ||
+      !(wait$ = SDL_CreateMutex()) || SDL_LockMutex(wait$))
     return printf("bad mem\n"), 1;
 
   if (av_opt_set(*dry, "filename", argv[argc - 1], 1) < 0 ||
@@ -32,16 +28,20 @@ int main(int argc, char **argv) {
       av_opt_set(*room, "filename", argv[argc - 2], 1) < 0 ||
       av_opt_set_int_list(*sink, "sample_fmts", ((int[]){1, -1}), -1, 1) < 0 ||
       av_opt_set(*verb, "weights", "20 10", 1) < 0 ||
-      avfilter_init_str(*dry, 0) < 0 || avfilter_init_str(*drysplit, 0) < 0 ||
+      avfilter_init_str(*band, 0) < 0 || avfilter_init_str(*dry, 0) < 0 ||
+      avfilter_init_str(*nul, 0) < 0 || avfilter_init_str(*nulsink, 0) < 0 ||
       avfilter_init_str(*room, 0) < 0 || avfilter_init_str(*sink, 0) < 0 ||
-      avfilter_init_str(*verb, 0) < 0 || avfilter_init_str(*verbwet, 0) < 0 ||
-      avfilter_link(*dry, 0, *drysplit, 0) < 0 ||
-      avfilter_link(*drysplit, 0, *verbwet, 0) < 0 ||
+      avfilter_init_str(*verb, 0) < 0 || avfilter_init_str(*verbsplit, 0) < 0 ||
+      avfilter_init_str(*verbwet, 0) < 0 ||
+      avfilter_link(*nul, 0, *nulsink, 0) < 0 ||
+      avfilter_link(*dry, 0, *band, 0) < 0 ||
+      avfilter_link(*band, 0, *verbsplit, 0) < 0 ||
+      avfilter_link(*verbsplit, 0, *verbwet, 0) < 0 ||
       avfilter_link(*room, 0, *verbwet, 1) < 0 ||
-      avfilter_link(*drysplit, 1, *verb, 0) < 0 ||
+      avfilter_link(*verbsplit, 1, *verb, 0) < 0 ||
       avfilter_link(*verbwet, 0, *verb, 1) < 0 ||
       avfilter_link(*verb, 0, *sink, 0) < 0 ||
-      avfilter_graph_config(flt, 0) < 0)
+      avfilter_graph_config(TT.g, 0) < 0)
     return printf("bad graph\n"), 1;
 
   want.channels = av_buffersink_get_channels(*sink);
